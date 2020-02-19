ce37fce2fb9544698a4dd067558e90ccf15cba36
Ptarrant <ptarrant@gmail.com>
2020-02-18 21:34:22 -0500

    fixing pip_install.sh again

--- app/pip_install_req.sh
+++ app/pip_install_req.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-cd $1
+cd $1/crafty-web
 git checkout $2
-source ./venv/bin/activate
-pip3 install --no-cache-dir -r ./crafty-web/requirements.txt
+
+source ../venv/bin/activate
+pip3 install --no-cache-dir -r requirements.txt
 deactivate
