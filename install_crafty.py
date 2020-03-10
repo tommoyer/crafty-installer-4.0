66c2023fed78d6745141759c096e364798bc14f7
MCgamin1738 <5355146-adamreynolds152@users.noreply.gitlab.com>
2020-03-10 18:12:38 +0000

    Fixing Git Clone Link

--- install_crafty.py
+++ install_crafty.py
@@ -84,7 +84,7 @@ def setup_repo():
 
     # cloning the repo
     pretty.info("Cloning the Git Repo...this could take a few moments")
-    subprocess.check_output('git clone http://gitlab.com/Ptarrant1/crafty-web.git', shell=True)
+    subprocess.check_output('git clone http://gitlab.com/crafty-controller/crafty-web.git', shell=True)
 
 
 # this switches to the branch chosen and does the pip install and such
