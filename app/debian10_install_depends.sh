0c3aa0bf580dacc016fbe09ad07b734cfbb049aa
MCgamin1738 <5355146-MCgamin1738@users.noreply.gitlab.com>
2020-04-21 12:21:26 +0000

    Fix Debian 10 Support

--- app/debian10_install_depends.sh
+++ app/debian10_install_depends.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 sudo apt update -y
-sudo apt install git python3.7 python3.7-dev python3-pip software-properties-common default-jre openjdk-11-jdk openjdk-11-jdk-headless virtualenv
+sudo apt install git python3.7 python3.7-dev python3-pip software-properties-common default-jre openjdk-11-jdk openjdk-11-jdk-headless virtualenv -y
 pip3 install virtualenv
 sudo useradd crafty
