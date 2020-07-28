afe898e75d08bca934be0815a04d47749d55f557
Phillip Tarrant <ptarrant@gmail.com>
2020-07-28 15:05:56 -0400

    trying manu auto distro selection

--- install_crafty.sh
+++ install_crafty.sh
@@ -3,7 +3,7 @@ if [[ $EUID -ne 0 ]]; then
    echo "Apologies - This script must be run as root"
    exit 1
 else
-  sudo apt install python-pip -y
+  sudo apt install python-pip3 -y
   pip3 install distro
   python3 install_crafty.py
 fi
