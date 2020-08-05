6c5754764fbd41a41a1e18f15f472044b708a0f5
Manuel Vezina <manuel.vezina@gmail.com>
2020-08-05 16:02:44 +0000

    Dev

--- app/debian_10.sh
+++ app/debian_10.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 sudo apt update -y
-sudo apt install git python3.7 python3.7-dev python3-pip software-properties-common default-jre openjdk-11-jdk openjdk-11-jdk-headless virtualenv -y
-pip3 install virtualenv
+sudo apt install git python3 python3-dev python3-pip python3-venv software-properties-common openjdk-11-jdk openjdk-11-jre -y
 sudo useradd crafty -s /bin/bash
--- app/ubuntu_20_04.sh
+++ app/ubuntu_20_04.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 sudo apt update -y
-sudo apt install git python3.8 python3.8-dev python3.8-venv python3-pip software-properties-common openjdk-8-jdk openjdk-8-jre virtualenv -y
-pip3 install virtualenv
-sudo useradd crafty
+sudo apt install git python3 python3-dev python3-pip python3-venv software-properties-common openjdk-8-jdk openjdk-8-jre -y
+sudo useradd crafty -s /bin/bash
