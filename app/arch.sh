b5e2bb682589e8d88fefc7f3b7ef95d5fae56ba7
Quentin <quentin@quentb.com>
2021-12-02 13:50:10 -0500

    update arch install packages

--- app/arch.sh
+++ app/arch.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 sudo pacman -Syu
-sudo pacman -S --noconfirm git extra/python extra/python-pip extra/jre16-openjdk extra/jdk16-openjdk
+sudo pacman -S --noconfirm git extra/python extra/python-pip extra/jre-openjdk extra/jdk-openjdk
 sudo useradd crafty -s /bin/bash
