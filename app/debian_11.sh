4bc1830db7b4341e9f41c543175fd991aae434a7
Andrew <mcdeweykp@gmail.com>
2023-01-18 18:46:03 -0500

    Add libcurl4 to all apt distros for bedrock

--- app/debian_11.sh
+++ app/debian_11.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo apt update -y
-sudo apt install wget apt-transport-https git python3 python3-dev python3-pip python3-venv software-properties-common openjdk-11-jdk-headless openjdk-11-jre-headless openjdk-17-jdk-headless openjdk-17-jre-headless build-essential musl-dev libffi-dev rustc libssl-dev -y
+sudo apt install wget apt-transport-https git python3 python3-dev  python3-pip python3-venv libcurl4 software-properties-common openjdk-11-jdk-headless openjdk-11-jre-headless openjdk-17-jdk-headless openjdk-17-jre-headless build-essential musl-dev libffi-dev rustc libssl-dev -y
 
 # Install Temurin 8 (Formerly AdoptOpenJDK)- OpenJDK 8 is not in official repos as of Debian 11
 wget -O - https://packages.adoptium.net/artifactory/api/gpg/key/public | sudo tee /usr/share/keyrings/adoptium.asc
