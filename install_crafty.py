652f91b751dbba5cb1e31a1adecd8e09dd5faab3
Wout Bouckaert <me@woutb.com>
2023-06-26 11:38:00 -0600

    Remove EOL Ubuntu versions 21.04 and 21.10.

--- install_crafty.py
+++ install_crafty.py
@@ -61,15 +61,7 @@ def do_distro_install(distro):
         "Please be patient and do not exit the installer otherwise things may break"
     )
 
-    if distro == "ubuntu_21_04.sh":
-        pretty.info("We are updating python3, open-jdk and pip")
-        script = os.path.join(real_dir, "app", "ubuntu_21_04.sh")
-
-    elif distro == "ubuntu_21_10.sh":
-        pretty.info("We are updating python3, open-jdk and pip")
-        script = os.path.join(real_dir, "app", "ubuntu_21_10.sh")
-
-    elif distro == "ubuntu_22_04.sh":
+    if distro == "ubuntu_22_04.sh":
         pretty.info("We are updating python3, open-jdk and pip")
         script = os.path.join(real_dir, "app", "ubuntu_22_04.sh")
 
@@ -492,13 +484,7 @@ def get_distro():
             )
 
     elif id == "ubuntu":
-        if version == "21.04":
-            logger.info("Ubuntu 21.04 Detected")
-            file = "ubuntu_21_04.sh"
-        elif version == "21.10":
-            logger.info("Ubuntu 21.10 Detected")
-            file = "ubuntu_21_10.sh"
-        elif version == "22.04":
+        if version == "22.04":
             logger.info("Ubuntu 22.04 Detected")
             file = "ubuntu_22_04.sh"
 
@@ -511,8 +497,7 @@ def get_distro():
             file = "ubuntu_23_04.sh"
         else:
             logger.critical(
-                "Unsupported Ubuntu - We only support Ubuntu 21.04 / 21.10 /"
-                " 22.04 / 22.10 / 23.04"
+                "Unsupported Ubuntu - We only support Ubuntu 22.04 / 22.10 / 23.04"
             )
 
     elif id == "rocky":
