cbe427d5c2ef38cb6628ef3fc8cf59dbc7254787
Amethyst_Nightshade <11422345-Amethyst_Nightshade@users.noreply.gitlab.com>
2023-11-30 09:20:27 -0500

    Rocky use git-core minimal version

--- app/rocky.sh
+++ app/rocky.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 sudo dnf update -y
-sudo dnf install git python3 java-17-openjdk-headless -y
+sudo dnf install git-core python3 java-17-openjdk-headless -y
 sudo useradd crafty -s /bin/bash
